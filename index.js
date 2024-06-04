4d3488b270ee4ac7a2ff7af8345ef3c5dbf05394
Do Phuong <do783241@gmail.com>
2024-06-04 20:06:19 +0700

    Cai dat pug va tao foder view

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+
+app.set("views" , "./views");
+app.set("view engine" , "pug");
+
 const port = 3000;
 app.get("/" , (req , res)=>{
-    res.send("trang chu");
-
+    res.render("client/pages/home/index");
 });
 app.get("/products" , (req , res)=>{
-    res.send("trang danh sach san pham");
+    res.render("client/pages/products/index");
 });
 app.listen(port, ()=>{
     console.log(`App listenning on port ${port}`);
