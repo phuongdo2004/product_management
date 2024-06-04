6c92019b7ba44c111fa0ea2f9b42acfe2972e84a
Do Phuong <do783241@gmail.com>
2024-06-04 20:25:12 +0700

    cau hinh route

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require("express");
+const routeClient = require("./routes/client/index.route")
 const app = express();
 
 app.set("views" , "./views");
 app.set("view engine" , "pug");
 
 const port = 3000;
-app.get("/" , (req , res)=>{
-    res.render("client/pages/home/index");
-});
-app.get("/products" , (req , res)=>{
-    res.render("client/pages/products/index");
-});
+
+
+routeClient.index(app);
 app.listen(port, ()=>{
     console.log(`App listenning on port ${port}`);
 });
